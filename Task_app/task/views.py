a2daba322d29ebbfb3ca2c59447886d261d44ef2
NoosratJahan <131634101+NoosratJahan@users.noreply.github.com>
2023-10-15 22:01:28 +0600

    Update views.py

--- Task_app/task/views.py
+++ Task_app/task/views.py
@@ -1,17 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Task
 from .forms import TaskForm
-# Create your views here.
 
-def home(request):
-        
+
+def landing_page(request):
+    return render(request, 'landing.html')
+
+
+#User login
+def login_user(request):
     if request.method == 'POST':
-        form = TaskForm()
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You are successfully logged in.."))
+            return redirect('home/')
+        else:
+            messages.success(request, ("There was an error logging in. Try again..."))
+            return redirect('login')    
+    else:        
+        return render(request, 'login.html')
+
+
+#User logout
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out!.."))
+    return redirect('/')
+
+
+#User Register
+
+
+#Create
+def home(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
         item = Task.objects.all()
         if form.is_valid():
-            form.save()            
-            return render(request, 'home.html', {'item': item})
-
-    
+            form.save()
+            return redirect('task_view')         
+   
     item = Task.objects.all()
     return render(request, 'home.html', {'item': item})
+
+
+
+#Retrieve
+def task_view(request):
+    form = TaskForm(request.POST)
+    item = Task.objects.all()
+    if form.is_valid():
+        form.save()
+        return render(request, 'task_view.html', {'item': item})
+    return render(request, 'task_view.html', {'item': item})
+
+
+#Update
+def update(request, id):
+    pass
+
+
+#Delete
+def delete(request, id):
+    item = Task.objects.get(id=id)    
+    item.delete()
+    return redirect('task_view')
+
