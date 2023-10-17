7400ad1c801699671afebc1ba2e88d30e992e233
NoosratJahan <131634101+NoosratJahan@users.noreply.github.com>
2023-10-17 12:34:00 +0600

    Update urls.py

--- Task_app/task/urls.py
+++ Task_app/task/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [  
     path('', views.landing_page, name='landing'),
     path('login', views.login_user, name='login'),
+    path('register', views.register, name='register'),
     path('logout', views.logout_user, name='logout'),
     path('home/', views.home, name='home'),
     path('task_view/', views.task_view, name='task_view'),
-    path('delete/<int:id>/', views.delete, name='delete')
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
 ]
